cd7b5a643a1edc3833231acdcf4f5cd91f27bfe8
sean <seanbruce520@gmail.com>
2022-10-19 21:15:43 +0800

    some primitive value in C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = "sean";
+string language = "C#";
+Console.WriteLine($"{name} is learing a new programming language {language} which is awesome!");
+
+
+string stringWithSpace = "       Angular          ";
+Console.WriteLine($"****{stringWithSpace.Trim()}****");
+
+int number1 = 32;
+int number2 = 8;
+
+Console.WriteLine(number1 + number2);
+Console.WriteLine(number1 - number2);
+Console.WriteLine(number1 * number2);
+Console.WriteLine(number1 / number2);
+Console.WriteLine(number1 % number2);
+
+int max = int.MaxValue;
+int min = int.MinValue;
+
+Console.WriteLine(max);
+Console.WriteLine(min);
+
+Console.WriteLine($"ingeter overflow {max + 3}");
+
+double number3 = 9;
+double number4 = 2;
+double number5 = number3 / number4;
+Console.WriteLine(number5);
+
+double maxd = double.MaxValue;
+double mind = double.MinValue;
+Console.WriteLine(maxd);
+Console.WriteLine(mind);
+
+Console.WriteLine(Math.PI);
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # learnCSharpBasics
+
+1. to get the length of a string by `Length` property on string, notice the `L` of `Length` is uppercased.
+
+2. like in JavaScript, string type is immutable, meaning they can't be mutated. 
+
+3. It seems methods of object in C# is all capitalized. Is is required ?
+
+4. operation of type int always produce int outcome.
